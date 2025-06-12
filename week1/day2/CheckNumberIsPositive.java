5b6420c5dfd5c7c017e19522a5bcad99b35d0fcb
madhura-tk <95517451+madhura-tk@users.noreply.github.com>
2025-06-12 19:37:10 +0530

    Add files via upload

---
+++ week1/day2/CheckNumberIsPositive.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int version= 10;
+		if (version > 0) {
+			System.out.println("The Input Number "+version+" is Positive");
+		}
+		else {
+			System.out.println("The Input Number "+version+" is not Positive");
+
+		}
+			
+	}
+
+}
