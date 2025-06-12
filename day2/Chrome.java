37d17a1eae3c09241b2dd936c750b6d3e3aa1d8c
madhura-tk <95517451+madhura-tk@users.noreply.github.com>
2025-06-12 19:35:41 +0530

    day 2 assignments

---
+++ day2/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 float version = 91.0f;
+		 String name="Google";
+		 boolean isBeta =false;
+		 int releaseYear=2008;
+		 char shortcutKey='C';
+		 System.out.println("Version : "+version);
+		 System.out.println("Name : "+name);
+		 System.out.println("isBeta : "+isBeta);
+		 System.out.println("releaseYear : "+releaseYear);
+		 System.out.println("shortcutKey : "+shortcutKey);
+		
+	}
+
+}
