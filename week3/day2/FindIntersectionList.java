3ed484d1554cfa7a725744ba8da9f9387891e0af
madhura-tk <95517451+madhura-tk@users.noreply.github.com>
2025-06-25 08:58:15 +0530

    Update FindIntersectionList.java

--- week3/day2/FindIntersectionList.java
+++ week3/day2/FindIntersectionList.java
@@ -6,17 +6,19 @@ import java.util.List;
 
 public class FindIntersectionList {
 	public static void main(String[] args) {
-
+		//Initialising the integer arrays
 		Integer[] arr1 = { 3,2, 11, 4, 6, 7 };
 		Integer[] arr2 = { 1, 2, 8, 4, 9, 7 };
-
+		
+		//Adding arrays to the List
 		List<Integer> arrList1 = new ArrayList<>(Arrays.asList(arr1));
 		List<Integer> arrList2 = new ArrayList<>(Arrays.asList(arr2));
 
+		//Comparing two arrays,by checking the common values in the arrays
 		for (int i = 0; i < arrList1.size(); i++) {
 			int num = arrList1.get(i);
 			
-			if (arrList2.contains(num)) {
+			if (arrList2.contains(num)) { 
 				System.out.println(num);
 			}
 		}
